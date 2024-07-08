18a640a83e38880f61d77990b25b8dc8269cee7d
dominiquesangou <150062792+dominiquesangou@users.noreply.github.com>
2024-07-08 16:03:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ async def welcome(num:Number):
     return(f"Request confirmation: Product is {a+b}!")
  
 if __name__=='__main__':
-    uvicorn.run(app,host='0.0.0.0', port=8001)
+    uvicorn.run(app,host='0.0.0.0', port=8000)
